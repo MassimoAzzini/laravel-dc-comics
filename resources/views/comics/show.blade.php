90aa6397896fd2bcb4e28b50961922411066616d
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-11-24 17:39:43 +0100

    [add]: message confirm to delate (to fix)

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -24,7 +24,25 @@
         <p>{{ $comic->description }}</p>
         <div class="d-flex mt-3">
             <a href="{{ route('comics.edit', $comic) }}" class="btn btn-warning me-3">Edit</a>
-            @include('partials.buttonDelate')
+            {{-- @include('partials.buttonDelate') --}}
+            {{-- <form action="{{ route('comics.destroy', $comic) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete {{ $comic->title }}?')">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">
+                    Delate
+                </button>
+            </form> --}}
+
+
+            <form action="{{route('comics.destroy', $comic)}}" method="post" onsubmit="return confirm('Sicuro di voler eliminare questo Fumetto?')">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">
+                    Elimina Fumetto
+                </button>
+            </form>
+
+
         </div>
 
     </div>
--- resources/views/partials/buttonDelate.blade.php
+++ resources/views/partials/buttonDelate.blade.php
@@ -1,5 +1,7 @@
-<form action="{{ route('comics.destroy', $comic) }}" method="POST">
+<form action="{{ route('comics.destroy', $comic) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete {{ $comic->title }}?')">
     @csrf
     @method('DELETE')
-    <button type="submit" class="btn btn-danger">Delate</button>
+    <button type="submit" class="btn btn-danger">
+        Delate
+    </button>
 </form>
