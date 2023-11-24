44231525c839baa30cdc1480f64336fb680dac2d
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-11-24 17:58:39 +0100

    [edit] confirm delate (to fix)

--- resources/views/partials/buttonDelate.blade.php
+++ resources/views/partials/buttonDelate.blade.php
@@ -1,7 +1,7 @@
-<form action="{{ route('comics.destroy', $comic) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete {{ $comic->title }}?')">
+<form action="{{ route('comics.destroy', $comic) }}" method="POST">
     @csrf
     @method('DELETE')
-    <button type="submit" class="btn btn-danger">
+    <button type="submit" class="btn btn-danger" onsubmit="return confirm('Are you sure you want to delete {{ $comic->title }}?')">
         Delate
     </button>
 </form>
